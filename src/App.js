e6fd4ae40ff766cdd7727c2d210e8a9ea67257e4
Val Sab <valerie.sabando@gmail.com>
2023-09-11 17:18:40 +0200

    Finalize codes of project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
 export default function App() {
-  return <div>
-
-<div>`Generated Color: <background color hex code>`</div>
-<button></button>
-
-
-  </div>;
+  const newColor = randomColor();
+  const [color, setColor] = useState(newColor);
+  return (
+    <>
+      <div
+        style={{
+          fontSize: '35px',
+          display: 'flex',
+          alignItems: 'center',
+          border: '1px solid black',
+          margin: '50px',
+          backgroundColor: color,
+        }}
+      >
+        Generated Color: {color}
+      </div>
+      <button
+        onClick={() => {
+          const generateColor = randomColor();
+          setColor(generateColor);
+        }}
+        style={{
+          border: '1px solid black',
+          margin: '50px',
+        }}
+      >
+        Generate
+      </button>
+    </>
+  );
 }
