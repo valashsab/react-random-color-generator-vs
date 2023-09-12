9ef3dc85939110468e0c2bdc28bfb8eb3cfcad3d
Val Sab <valerie.sabando@gmail.com>
2023-09-12 16:03:10 +0200

    Add codes for hue and lunimosity input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,74 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function App() {
+  // create state for the button
+  const initialColor = randomColor();
+  const [color, setColor] = useState(initialColor);
+
+  /* create state for the input - nicht vollständig
+  const colorHue = initialColor.hue;
+  const [generatedColorHue, setGeneratedColorHue] = useState(colorHue);
+  })
+  */
+  const hue = randomColor({
+    hue: 'blue',
+  });
+
+  const luminosity = 'light';
+  return (
+    <>
+      <div
+        style={{
+          fontSize: '35px',
+          padding: '10px',
+          border: '1px solid black',
+          borderRadius: '10px',
+          position: 'realtive',
+          margin: '10px',
+          backgroundColor: color,
+        }}
+      >
+        {/* try if function in css is possible
+original:    Generated Color: {color}
+      */}
+        Generated Color:{color};
+      </div>
+
+      <input
+        style={{
+          fontSize: '15px',
+          padding: '10px',
+          border: '1px solid black',
+          borderRadius: '10px',
+          margin: '10px',
+          backgroundColor: '#ffffff',
+        }}
+        value={generatedColorHue}
+        onChange={(event) => setGeneratedColorHue(event.currentTarget.value)}
+      />
+      <br />
+      <button
+        onClick={() => {
+          const newColor = randomColor();
+          setColor(newColor);
+        }}
+        style={{
+          fontSize: '15px',
+          padding: '10px',
+          border: '1px solid black',
+          borderRadius: '10px',
+          margin: '10px',
+          backgroundColor: '#ffffff',
+        }}
+      >
+        Generate
+      </button>
+    </>
+  );
+}
+
+/* working function generating random color generator
+export default function App() {
   const initialColor = randomColor();
   const [color, setColor] = useState(initialColor);
   return (
@@ -38,3 +106,4 @@ export default function App() {
     </>
   );
 }
+*/
