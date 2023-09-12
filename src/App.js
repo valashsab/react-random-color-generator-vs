2b1e3651eac51b58cee0393a4d41383dfa78e87e
Val Sab <valerie.sabando@gmail.com>
2023-09-12 20:28:32 +0200

    Add input value

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+// import { argv } from 'node:process';
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
+// generate random color generator via useState
 export default function App() {
-  // create state for the button
   const initialColor = randomColor();
   const [color, setColor] = useState(initialColor);
 
-  /* create state for the input - nicht vollständig
-  const colorHue = initialColor.hue;
-  const [generatedColorHue, setGeneratedColorHue] = useState(colorHue);
-  })
-  */
   const hue = randomColor({
-    hue: 'blue',
+    hue: 'random',
   });
 
-  const luminosity = 'light';
+  const [newHue, setNewHue] = useState(hue);
+
   return (
     <>
       <div
         style={{
+          font: 'Courier New',
           fontSize: '35px',
           padding: '10px',
           border: '1px solid black',
@@ -29,14 +27,11 @@ export default function App() {
           backgroundColor: color,
         }}
       >
-        {/* try if function in css is possible
-original:    Generated Color: {color}
-      */}
-        Generated Color:{color};
+        Generated Color: {color}
       </div>
-
       <input
         style={{
+          font: 'Courier New',
           fontSize: '15px',
           padding: '10px',
           border: '1px solid black',
@@ -44,8 +39,10 @@ original:    Generated Color: {color}
           margin: '10px',
           backgroundColor: '#ffffff',
         }}
-        value={generatedColorHue}
-        onChange={(event) => setGeneratedColorHue(event.currentTarget.value)}
+        value={newHue}
+        oneChange={(event) => {
+          setNewHue(event.currentTarget.value);
+        }}
       />
       <br />
       <button
@@ -54,7 +51,8 @@ original:    Generated Color: {color}
           setColor(newColor);
         }}
         style={{
-          fontSize: '15px',
+          font: 'courier new',
+          fontSize: '1=px',
           padding: '10px',
           border: '1px solid black',
           borderRadius: '10px',
@@ -68,7 +66,9 @@ original:    Generated Color: {color}
   );
 }
 
-/* working function generating random color generator
+/*
+
+// working function generating random color generator
 export default function App() {
   const initialColor = randomColor();
   const [color, setColor] = useState(initialColor);
@@ -106,4 +106,30 @@ export default function App() {
     </>
   );
 }
+
+*/
+
+/* create state for the input - nicht vollständig
+  const requestedColor = initialColor({ hue: argv[2], luminosity: argv[3] });
+  const [generatedColor, setGeneratedColor] = useState(requestedColor);
+  }
+
+
+  // 1.variable = input typed in browser console.log
+  // [x] variable = luminosity
+  // [x] variable = hue
+  /* 4. connection between randomColor and transformed in hue and luminosity - meaning input value === */
+
+/* entered input should color the div-text to the requested color
+1. input field
+2.
+a. entered color = new variable = enteredColor =
+b. randomColor transformed in hue and luminosity
+3.
+a. enteredColor is a dynamic variable meaning state changes with new request -->
+b. const [generatedColor, setGeneratedColor] = useState(enteredColor);
+c. generatedColor should be visible in the div - on change therefore in div-container
+
+[]or create new function for hue
+[] create new function for
 */
